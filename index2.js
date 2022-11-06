47b57136da550b43c052d2738c43bd4fb2141f92
Pharamesh B <109457510+Pharamesh@users.noreply.github.com>
2022-11-06 21:57:04 +0530

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,17 @@
+// create an ajax request to localhost:5000
+$.ajax({
+    url: 'http://localhost:5000',
+    type: 'GET',
+    success: function(data) {
+      data.forEach(element => {
+        let str = `<div class="card1 border m-2">
+        <img class="card-img-top" src="${element.link}" alt="Card image" style="width:100%">
+        <div class="card-body">
+          <h4 class="card-title name">Gif By BB17G</h4>
+          <span><span class="count">0</span> Likes ❤️</span>
+        </div>
+      </div>`
+       $('.gifs').append(str);
+        });
+    }
+});
